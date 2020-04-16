923c3d6e421da8148226140471103160a4a718d6
Vu Ngoc Tung <tungvn.0427@gmail.com>
2020-04-16 16:58:19 +0700

    Make Timesheets migration

---
+++ database/migrations/2020_04_16_085841_create_timesheets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimesheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->json('doing');
+            $table->text('problem');
+            $table->text('plan');
+            $table->string('status')->default(\App\Timesheet::STATUS_CREATED);
+            $table->uuid('created_by')->nullable();
+            $table->uuid('updated_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timesheets');
+    }
+}
