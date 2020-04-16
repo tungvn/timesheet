f5aef960c10ec0b63469a8aabddc6700fb830998
Vu Ngoc Tung <tungvn.0427@gmail.com>
2020-04-16 15:33:08 +0700

    Modify User model

--- app/User.php
+++ app/User.php
@@ -2,13 +2,23 @@
 
 namespace App;
 
+use App\Timesheets\Authoring\HasAuthors;
+use App\Timesheets\Traits\HasUuid;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, HasUuid, HasApiTokens, HasAuthors, SoftDeletes;
+
+    /** @var string */
+    const ROLE_ADMIN = 'admin';
+
+    /** @var string */
+    const ROLE_USER = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +26,14 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username',
+        'email',
+        'password',
+        'email_verified_at',
+        'leader_id',
+        'role',
+        'avatar',
+        'description',
     ];
 
     /**
@@ -25,7 +42,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -36,4 +54,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * This account belongs to a leader
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function leader()
+    {
+        return $this->belongsTo(self::class);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
 }
