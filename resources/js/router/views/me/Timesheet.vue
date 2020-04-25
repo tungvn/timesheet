bb05179ff3e816d838cf952778a4d73cf0b97fa6
Vu Ngoc Tung <tungvn.0427@gmail.com>
2020-04-25 15:31:04 +0700

    Create UI for my table timesheets

--- resources/js/router/views/me/Timesheet.vue
+++ resources/js/router/views/me/Timesheet.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="col-12">
-        <timesheet-table
-            :api-url="api.myTimesheet"
-            :delete-func="deleteItem"
-            :fields="fields"
-            :link-to="(id) => `/timesheet/${id}`"
-        />
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">My Timesheet</h3>
+            </div>
+            <div class="card-body p-0">
+                <timesheet-table
+                    :api-url="api.myTimesheet"
+                    :delete-func="deleteItem"
+                    :fields="fields"
+                    :link-to="(id) => `/me/timesheet/${id}`"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,15 +33,15 @@
                         name: 'date',
                     },
                     {
-                        name: 'created_at',
-                        title: 'Created At',
-                    },
-                    {
                         name: 'approved_at',
                         title: 'Approved',
                         formatter: (value) => {
                             return value ? 'Approved' : '';
                         },
+                    },
+                    {
+                        name: 'created_at',
+                        title: 'Created At',
                     },
                     {
                         name: 'actions',
