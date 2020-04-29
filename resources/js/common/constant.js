b625027eac959379fdfd1129e9ada98e7ee144bc
Vu Ngoc Tung <tungvn.0427@gmail.com>
2020-04-29 10:29:01 +0700

    Allow admin can change role on the UI

--- resources/js/common/constant.js
+++ resources/js/common/constant.js
@@ -1,5 +1,9 @@
 export const ROLE_ADMIN = 'admin';
 export const ROLE_USER = 'user';
+export const ROLES = {
+    [ROLE_USER]: 'User',
+    [ROLE_ADMIN]: 'Administrator',
+};
 
 export const STATUS_APPROVED = 'approved';
 export const STATUS_CREATED = 'created';
